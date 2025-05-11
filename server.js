4bf4f5fe299c390df4efeb4c8ac7246b15527bec
adil-km <adilkmparappur@gmail.com>
2025-05-11 18:58:58 +0530

    express route initiated

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/api/contacts',(req,res)=>{
+
+    res.status(200).json({message:"Getting all the contacts"})
+})
+
+app.listen(port,()=>{   
+    console.log(`server is running on port ${port}`);
+
+});
