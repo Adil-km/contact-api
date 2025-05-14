bd3c278c5323eacfab7c7923b3d146ace82dedb8
adil-km <adilkmparappur@gmail.com>
2025-05-14 19:15:14 +0530

    Express - Throw Error

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 
@@ -6,8 +7,9 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use('/api/contacts',require('./routes/contactRoutes'))
-
+app.use(errorHandler);
 app.listen(port,()=>{   
     console.log(`server is running on port ${port}`);
 
