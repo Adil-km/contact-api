cafb362d23df745ded4ded3888488531f7a4e964
adil-km <adilkmparappur@gmail.com>
2025-05-18 11:03:51 +0530

    JWT Access Token & User Login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 const User = require("../models/userModel");
 
 
@@ -42,7 +43,31 @@ const registerUser = asyncHandler(async (req,res) => {
 //@route POST /api/users/register
 //@access public
 const loginUser = asyncHandler(async (req,res) => {
-    res.json({message: "Login the user"});
+    const { email, password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory! ");
+    }
+
+    const user = await User.findOne({ email });
+
+    if(user && (await bcrypt.compare(password, user.password))){
+        const  accessToken = jwt.sign(
+        {
+            user:{  
+                username: user.username,
+                email: user.email,
+                id: user.id,
+                },
+        },
+        process.env.ACCESS_TOKEN_SECRETE,
+        {expiresIn: "1m"}
+        );
+        res.status(200).json({ accessToken });
+    }else{
+        res.status(401);
+        throw new Error("Email or password is not valid")
+    }
 });
 
 //@desc Current user info
